2d47599630b56b0079a247dd84de5879f29c9450
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-02-23 19:02:06 +0500

    Update Orders Analysis

--- Order Analysis.sql
+++ Order Analysis.sql
@@ -1,23 +1,30 @@
 -- select * from production.products
 
---Customer Details against Orders
+--Complete Orders and Customer Details   
 SELECT	
 		ord.order_id,
 		CONCAT(cus.first_name, ' ',cus.last_name) AS 'Customer Name',
 		cus.city,
 		cus.state,
-		ord.order_date
-		SUM(ite.quantity) AS 'Total Units',
-		SUM(ite.quantity * ite.list_price) AS 'Revenue'
+		ord.order_date,
+		SUM(it.quantity) AS 'Total Units',
+		SUM(it.quantity * it.list_price) AS 'Revenue',
+		pp.product_name
 FROM sales.orders ord
 JOIN sales.customers cus
 ON ord.customer_id =  cus.customer_id
-JOIN sales.order_items ite
-ON ord.order_id = ite.order_id
+JOIN sales.order_items it
+ON ord.order_id = it.order_id
+JOIN production.products pp 
+ON it.product_id = pp.product_id
 GROUP BY  
 		ord.order_id,
-		CONCAT(cus.first_name, ' ',cus.last_name) AS 'Customer Name',
+		CONCAT(cus.first_name, ' ',cus.last_name),
 		cus.city,
 		cus.state,
-		ord.order_date
+		ord.order_date,
+		pp.product_name
 ORDER BY 1 asc
+
+
+select * from sales.order_items
