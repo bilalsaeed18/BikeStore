f6f5f41f351453b4e348aff095e9d36f9e0724dc
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-02-25 18:22:53 +0500

    Update Order Analysis

--- Order Analysis.sql
+++ Order Analysis.sql
@@ -9,7 +9,10 @@ SELECT
 		ord.order_date,
 		SUM(it.quantity) AS 'Total Units',
 		SUM(it.quantity * it.list_price) AS 'Revenue',
-		pp.product_name
+		pp.product_name,
+		cat.category_name,
+		ss.store_name,
+		CONCAT(sals.first_name, ' ', sals.last_name) AS 'Sales Representative'
 FROM sales.orders ord
 JOIN sales.customers cus
 ON ord.customer_id =  cus.customer_id
@@ -17,13 +20,22 @@ JOIN sales.order_items it
 ON ord.order_id = it.order_id
 JOIN production.products pp 
 ON it.product_id = pp.product_id
+JOIN production.categories cat 
+ON pp.category_id = cat.category_id
+JOIN sales.stores ss
+ON ss.store_id = ord.store_id
+JOIN sales.staffs sals
+ON sals.staff_id = ord.staff_id
 GROUP BY  
 		ord.order_id,
 		CONCAT(cus.first_name, ' ',cus.last_name),
 		cus.city,
 		cus.state,
 		ord.order_date,
-		pp.product_name
+		pp.product_name,
+		cat.category_name,
+		CONCAT(sals.first_name, ' ', sals.last_name),
+		ss.store_name
 ORDER BY 1 asc
 
 
